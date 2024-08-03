8109a18e409cc28b4306dcaff62acf2a6be32994
r3k4r <rekarjamal33@gmail.com>
2024-08-03 12:20:47 +0300

    k

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
- 
+
 const nextConfig = {
   experimental: {
+    reactRoot: true,
+    serverComponents: true,
     ppr: 'incremental',
   },
 };
- 
+
 export default nextConfig;
