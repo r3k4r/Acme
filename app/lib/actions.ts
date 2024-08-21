204209b4d4f91d291751d46e658192acd589c147
r3k4r <rekarjamal33@gmail.com>
2024-08-21 18:20:01 +0300

    k

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -72,6 +72,7 @@ export async function updateInvoice(id: string, formData: FormData) {
 
 
 export async function deleteInvoice(id: string) {
+
   try {
     await sql`DELETE FROM invoices WHERE id = ${id}`;
     revalidatePath('/dashboard/invoices');
